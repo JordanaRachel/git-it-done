128bb36fe3d69981fce87f356e37b4105d14b106
unknown <jordanabravo2009@yahoo.com>
2021-07-20 22:57:11 -0700

    errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,26 +1,57 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var repoSearchTerm = document.querySelector('#repo-search-term');
+
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+  
+    // get value from input element
+    var username = nameInputEl.value.trim();
+  
+    if (username) {
+      getUserRepos(username);
+  
+      // clear old content
+      repoContainerEl.textContent = '';
+      nameInputEl.value = '';
+    } else {
+      alert('Please enter a GitHub username');
+    }
+  };
 
 var getUserRepos = function(user) {
-
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";  
+   
     // make a request to the url
     fetch(apiUrl)
   .then(function(response) {
     // request was successful
     if (response.ok) {
+        console.log(response);  
       response.json().then(function(data) {
+        console.log(data);
         displayRepos(data, user);
       });
     } else {
-      alert('Error: GitHub User Not Found');
+        alert('Error: ' + response.statusText);
     }
-  })
-  .catch(function(error) {
-    // Notice this `.catch()` getting chained onto the end of the `.then()` method
-    alert("Unable to connect to GitHub");
+    })
+    .catch(function(error) {
+        alert("Unable to connect to GitHub");
   });
+  };
+
+  var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+      repoContainerEl.textContent = 'No repositories found.';
+      return;
+    }
+
+    repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
@@ -39,52 +70,24 @@ var getUserRepos = function(user) {
     repoEl.appendChild(titleEl);
     
     // create a status element
-var statusEl = document.createElement("span");
-statusEl.classList = "flex-row align-center";
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+    statusEl.innerHTML =
+      "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+    } else {
+    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
 
     // append to container
-repoEl.appendChild(statusEl);
+    repoEl.appendChild(statusEl);
 
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
-  }
-      }
-  var userFormEl = document.querySelector("#user-form");
-  var nameInputEl = document.querySelector("#username");
-  var formSubmitHandler = function(event) {
-    event.preventDefault();
- // get value from input element
-var username = nameInputEl.value.trim();
-
-if (username) {
-  getUserRepos(username);
-  nameInputEl.value = "";
-} else {
-  alert("Please enter a GitHub username");
-}   
-    console.log(event);
-  };
-
-  // check if api returned any repos
-if (repos.length === 0) {
-    repoContainerEl.textContent = "No repositories found.";
-    return;
-  }
+    }
+};
   
-// check if current repo has issues or not
-if (repos[i].open_issues_count > 0) {
-    statusEl.innerHTML =
-      "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-  } else {
-    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-  }
-  var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
-  };
-
-  userFormEl.addEventListener("submit", formSubmitHandler);
-
-  // clear old content
-repoContainerEl.textContent = "";
-repoSearchTerm.textContent = searchTerm;
+ // add event listeners to forms
+userFormEl.addEventListener('submit', formSubmitHandler);
